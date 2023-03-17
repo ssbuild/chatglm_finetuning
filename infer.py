184ab7be4d1c26b322fda601ceac2a6fad2448af
tk <462304@qq.com>
2023-03-17 21:03:31 +0800

    update
    Signed-off-by: tk <462304@qq.com>

--- infer.py
+++ infer.py
@@ -6,7 +6,7 @@ from deep_training.data_helper import ModelArguments, TrainingArguments, DataArg
 from deep_training.nlp.models.chatglm import TransformerChatGlmLMHeadModel, setup_model_profile, ChatGLMConfig,ChatGLMForConditionalGeneration
 from transformers import HfArgumentParser
 
-from data_utils import train_info_args, NN_DataHelper
+from data_utils import train_info_args, NN_DataHelper,get_deepspeed_config
 from tokenization_chatglm import ChatGLMTokenizer
 
 
@@ -29,10 +29,11 @@ if __name__ == '__main__':
 
 
     #加载新训练权重
-    train_weight = './best_ckpt/last.pt'
-
-    #deepspeed权重
-    #train_weight = './best_ckpt/last.ckpt/checkpoint/mp_rank_00_model_states.pt'
+    if get_deepspeed_config is None:
+        train_weight = './best_ckpt/last.ckpt'
+    else:
+        #deepspeed权重
+        train_weight = './best_ckpt/last.ckpt/checkpoint/mp_rank_00_model_states.pt'
     if os.path.exists(train_weight):
         config = ChatGLMConfig.from_pretrained('./best_ckpt')
         try:
