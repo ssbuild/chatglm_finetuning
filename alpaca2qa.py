f0259ee47acf7996d1da242d3fee3595b3ec08ff
ssbuild <462304@qq.cn>
2023-03-24 10:09:30 +0800

    update 模板
    Signed-off-by: ssbuild <462304@qq.cn>

--- alpaca2qa.py
+++ alpaca2qa.py
@@ -29,12 +29,12 @@ PROMPT_DICT = {
     "prompt_input": (
         "下面是一个指令,描述了一个任务,搭配一个输入,提供进一步的上下文。"
         "编写适当的输出完成请求。\n\n"
-        "### 指令:\n{instruction}\n\n### 输入:\n{input}\n\n### 输出:"
+        "### 指令:\n{instruction}\n\n### 请求:\n{input}\n\n### 响应:"
     ),
     "prompt_no_input": (
         "下面是一个指令,描述了一个任务。"
         "编写适当的输出完成请求。\n\n"
-        "### 指令:\n{instruction}\n\n### 输出:"
+        "### 指令:\n{instruction}\n\n### 响应:"
     ),
 }
 
