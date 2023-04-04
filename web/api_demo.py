714bd98fd2c691f0fab2055a3972c4d039103a48
ssbuild <462304@qq.cn>
2023-04-04 14:50:29 +0800

    web demo
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ web/api_demo.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2023/4/4 14:46
+
+from fastapi import FastAPI, Request
+import uvicorn, json, datetime
+import torch
+
+from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
+from deep_training.nlp.models.chatglm import setup_model_profile, ChatGLMConfig
+from deep_training.nlp.models.lora import LoraArguments
+from transformers import HfArgumentParser
+
+from data_utils import train_info_args, NN_DataHelper
+from models import MyTransformer,ChatGLMTokenizer
+
+
+DEVICE = "cuda"
+DEVICE_ID = "0"
+CUDA_DEVICE = f"{DEVICE}:{DEVICE_ID}" if DEVICE_ID else DEVICE
+
+
+def torch_gc():
+    if torch.cuda.is_available():
+        with torch.cuda.device(CUDA_DEVICE):
+            torch.cuda.empty_cache()
+            torch.cuda.ipc_collect()
+
+
+app = FastAPI()
+
+
+@app.post("/")
+async def create_item(request: Request):
+    global model, tokenizer
+    json_post_raw = await request.json()
+    json_post = json.dumps(json_post_raw)
+    json_post_list = json.loads(json_post)
+    prompt = json_post_list.get('prompt')
+    history = json_post_list.get('history')
+    max_length = json_post_list.get('max_length')
+    top_p = json_post_list.get('top_p')
+    temperature = json_post_list.get('temperature')
+    response, history = model.chat(tokenizer,
+                                   prompt,
+                                   history=history,
+                                   max_length=max_length if max_length else 2048,
+                                   top_p=top_p if top_p else 0.7,
+                                   temperature=temperature if temperature else 0.95)
+    now = datetime.datetime.now()
+    time = now.strftime("%Y-%m-%d %H:%M:%S")
+    answer = {
+        "response": response,
+        "history": history,
+        "status": 200,
+        "time": time
+    }
+    log = "[" + time + "] " + '", prompt:"' + prompt + '", response:"' + repr(response) + '"'
+    print(log)
+    torch_gc()
+    return answer
+
+
+if __name__ == '__main__':
+    train_info_args['seed'] = None
+    parser = HfArgumentParser((ModelArguments, TrainingArguments, DataArguments, LoraArguments))
+    model_args, training_args, data_args, _ = parser.parse_dict(train_info_args)
+
+    setup_model_profile()
+
+    dataHelper = NN_DataHelper(model_args, training_args, data_args)
+    tokenizer: ChatGLMTokenizer
+    tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
+        tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
+
+    config = ChatGLMConfig.from_pretrained('./best_ckpt')
+    config.initializer_weight = False
+
+    lora_args = LoraArguments.from_pretrained('./best_ckpt')
+
+    assert lora_args.inference_mode == True and config.pre_seq_len is None
+
+    pl_model = MyTransformer(config=config, model_args=model_args, training_args=training_args, lora_args=lora_args)
+    # 加载lora权重
+    pl_model.backbone.from_pretrained(pl_model.backbone.model, pretrained_model_name_or_path='./best_ckpt',
+                                      lora_config=lora_args)
+
+    model = pl_model.get_glm_model()
+    # 按需修改
+    model.half().to(torch.cuda.device(CUDA_DEVICE))
+    model = model.eval()
+
+
+    model.eval()
+    uvicorn.run(app, host='0.0.0.0', port=8000, workers=1)
