f10a9635315331ac70a9f22078dd288c054fd2ff
tk <462304@qq.com>
2023-05-12 20:30:28 +0800

    change directory
    Signed-off-by: tk <462304@qq.com>

--- infer.py
+++ infer.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2023/3/9 15:29
+import sys
+sys.path.append('..')
 from deep_training.data_helper import ModelArguments, TrainingArguments, DataArguments
-from deep_training.nlp.models.chatglm import setup_model_profile, ChatGLMConfig
-from deep_training.nlp.models.lora.v2 import LoraArguments
 from transformers import HfArgumentParser
-
 from data_utils import train_info_args, NN_DataHelper
-from models import MyTransformer,ChatGLMTokenizer
+from models import MyTransformer,ChatGLMTokenizer,LoraArguments,setup_model_profile, ChatGLMConfig
 
 if __name__ == '__main__':
     train_info_args['seed'] = None
--- models/chatglm_model.py
+++ models/chatglm_model.py
@@ -1,17 +1,20 @@
-# @Time    : 2023/3/28 21:56
+# coding=utf8
+# @Time    : 2023/5/12 20:20
 # @Author  : tk
+# @FileName: chatglm_model
+
 import copy
 import os
 import re
 import warnings
 from typing import List, Tuple, Optional, Callable
 import torch
-from deep_training.nlp.models.chatglm import ChatGLMForConditionalGeneration,ChatGLMConfig, logger
+from deep_training.nlp.models.chatglm import ChatGLMForConditionalGeneration,ChatGLMConfig, logger,setup_model_profile
 from deep_training.nlp.models.lora.v2 import LoraArguments, LoraModel
 from deep_training.nlp.models.transformer import TransformerBase
 from torch import nn
 from transformers import LogitsProcessorList, LogitsProcessor, GenerationConfig, StoppingCriteriaList
-from tokenization_chatglm import ChatGLMTokenizer
+from models.tokenization_chatglm import ChatGLMTokenizer
 
 #如果显卡支持int8 可以开启 ， 需安装依赖 pip install bitsandbytes
 load_in_8bit = False
@@ -29,9 +32,7 @@ class InvalidScoreLogitsProcessor(LogitsProcessor):
 class MyChatGLMForConditionalGeneration(ChatGLMForConditionalGeneration):
     def __init__(self,config):
         super(MyChatGLMForConditionalGeneration, self).__init__(config)
-        if load_in_8bit:
-            setattr(self, 'model_parallel', True)
-            setattr(self, 'is_parallelizable', True)
+
 
     @torch.no_grad()
     def chat(self, tokenizer, query: str, history: List[Tuple[str, str]] = None, max_length: int = 2048, num_beams=1,
@@ -190,55 +191,11 @@ class MyChatGLMForConditionalGeneration(ChatGLMForConditionalGeneration):
 class MyTransformerChatGlmLMHeadModel(TransformerBase):
     def __init__(self, *args,**kwargs):
         #如果显卡支持int8 可以开启 ， 需安装依赖 pip install bitsandbytes
-        if load_in_8bit:
-            kwargs.update({"load_in_8bit": True,"device_map": "auto"})
+        load_in_8bit = kwargs.get('load_in_8bit',False)
+        if not load_in_8bit:
+            kwargs.pop("device_map",None)
         super(MyTransformerChatGlmLMHeadModel, self).__init__(*args,**kwargs)
         self.set_model(self.from_pretrained(MyChatGLMForConditionalGeneration, *args, **kwargs))
-
-
-class MyTransformer(MyTransformerChatGlmLMHeadModel, with_pl=True):
-    def __init__(self, *args, **kwargs):
-        lora_args: LoraArguments = kwargs.pop('lora_args',None)
-        super(MyTransformer, self).__init__(*args, **kwargs)
-        self.lora_args = lora_args
-
-        if lora_args is not None and lora_args.with_lora:
-            model = LoraModel(self.backbone, lora_args)
-            print('*' * 30,'lora info')
-            model.print_trainable_parameters()
-            self.set_model(model, copy_attr=False)
-        elif global_num_layers_freeze > 0 and self.config.pre_seq_len is None:  # 非 lora freeze 非 ptuning模式
-            M: nn.Module = self.backbone
-            for param in M.named_parameters():
-                result = re.match(re.compile('.*transformer.layers.(\\d+)'),param[0])
-                if result is not None:
-                    n_layer = int(result.group(1))
-                    if n_layer < global_num_layers_freeze:
-                        param[1].requires_grad = False
-                        print('freeze layer',param[0])
-
-
-    def get_glm_model(self) -> MyChatGLMForConditionalGeneration:
-        if self.lora_args is not None and self.lora_args.with_lora:
-            return self.backbone.model.model
-        return self.backbone.model
-
-    def save_pretrained_merge_lora(self,weight_path_file: str):
-        assert not load_in_8bit , ValueError('load_in_8bit is not support merge')
-        assert os.path.exists(os.path.dirname(weight_path_file))
-        assert self.lora_args is not None and self.lora_args.with_lora
-        lora_model : LoraModel = self.backbone
-        model: nn.Module = lora_model.merge_and_unload()
-        #保存hf权重，可用infer.py推理
-        torch.save(model.model.state_dict(),weight_path_file)
-        return model
-
-    def save_pretrained_merge_lora_and_restore(self, weight_path_file: str):
-        assert not load_in_8bit, ValueError('load_in_8bit is not support merge')
-        assert os.path.exists(os.path.dirname(weight_path_file))
-        assert self.lora_args is not None and self.lora_args.with_lora
-        lora_model: LoraModel = self.backbone
-        lora_model.merge_adapter()
-        # 保存hf权重，可用infer.py推理
-        torch.save(lora_model.model.model.state_dict(), weight_path_file)
-        lora_model.unmerge_adapter()
+        if load_in_8bit:
+            setattr(self.backbone, 'model_parallel', True)
+            setattr(self.backbone, 'is_parallelizable', True)
