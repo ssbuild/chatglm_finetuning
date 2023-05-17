292c896a614a2539978a28f852d457a90e0f8e82
ssbuild <462304@qq.cn>
2023-05-17 08:53:20 +0800

    fix bf16
    Signed-off-by: ssbuild <462304@qq.cn>

--- train.py
+++ train.py
@@ -144,7 +144,8 @@ if __name__ == '__main__':
     pl_model = MyTransformer(config=config, model_args=model_args, training_args=training_args, lora_args=lora_args,
                              load_in_8bit=global_load_in_8bit, device_map={"": trainer.local_rank} if trainer.world_size > 1 else "auto")
 
-    pl_model.bfloat16()
+    if not global_load_in_8bit:
+        pl_model.bfloat16()
 
     ckpt_path = './best_ckpt/best.pt'
     if not data_args.convert_onnx:
