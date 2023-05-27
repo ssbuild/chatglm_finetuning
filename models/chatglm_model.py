502254327087de5710a436c79655dce13473630f
ssbuild <462304@qq.com>
2023-05-27 19:51:18 +0800

    fix
    Signed-off-by: ssbuild <462304@qq.com>

--- models/chatglm_model.py
+++ models/chatglm_model.py
@@ -236,7 +236,7 @@ class MyTransformerChatGlmLMHeadModel(TransformerBase):
         load_in_8bit = kwargs.get('load_in_8bit', False)
         load_in_4bit = kwargs.get('load_in_4bit', False)
         if not load_in_4bit:
-            load_in_4bit = kwargs.get("quantization_config", {}).get('load_in_4bit', False)
+            load_in_4bit = (kwargs.get("quantization_config", {}) or {}).get('load_in_4bit', False)
 
         if not load_in_8bit and not load_in_4bit:
             kwargs.pop("device_map", None)
