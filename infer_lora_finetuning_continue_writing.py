73f2996efd645ce3dad7ea24c93e873c1429d690
ssbuild <462304@qq.cn>
2023-09-04 09:24:38 +0800

    update
    Signed-off-by: ssbuild <462304@qq.cn>

---
+++ infer_lora_finetuning_continue_writing.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+# @Author  : ssbuild
+# @Time    : 2023/9/4 9:22
+import os
+import torch
+from deep_training.data_helper import ModelArguments
+from transformers import HfArgumentParser
+from data_utils import train_info_args, NN_DataHelper,global_args
+from aigc_zoo.model_zoo.chatglm.llm_model import MyTransformer,ChatGLMTokenizer,setup_model_profile, ChatGLMConfig,PetlArguments
+
+
+if __name__ == '__main__':
+    train_info_args['seed'] = None
+    train_info_args['seed'] = None
+    parser = HfArgumentParser((ModelArguments,))
+    (model_args,) = parser.parse_dict(train_info_args, allow_extra_keys=True)
+    setup_model_profile()
+    dataHelper = NN_DataHelper(model_args)
+    tokenizer: ChatGLMTokenizer
+    tokenizer, _, _, _ = dataHelper.load_tokenizer_and_config(
+        tokenizer_class_name=ChatGLMTokenizer, config_class_name=ChatGLMConfig)
+
+    ckpt_dir = './best_ckpt/last'
+    config = ChatGLMConfig.from_pretrained(ckpt_dir)
+    config.initializer_weight = False
+    lora_args = PetlArguments.from_pretrained(ckpt_dir)
+
+    assert lora_args.inference_mode == True and config.pre_seq_len is None
+
+    new_num_tokens = config.vocab_size
+    if config.task_specific_params is not None and config.task_specific_params.get('vocab_size', None) is not None:
+        config.vocab_size = config.task_specific_params['vocab_size']
+
+    pl_model = MyTransformer(config=config, model_args=model_args, lora_args=lora_args,
+                             torch_dtype=torch.float16,new_num_tokens=new_num_tokens,
+                             # load_in_8bit=global_args["load_in_8bit"],
+                             # # device_map="auto",
+                             # device_map = {"":0} # 第一块卡
+                             )
+    # 加载lora权重
+    pl_model.load_sft_weight(ckpt_dir)
+
+    pl_model.eval().half().cuda()
+
+    enable_merge_weight = False
+    if enable_merge_weight:
+        # 合并lora 权重 保存
+        pl_model.save_sft_weight(os.path.join(ckpt_dir,'pytorch_model_merge.bin'),merge_lora_weight=True)
+
+    else:
+        model = pl_model.get_llm_model()
+
+        text_list = [
+            "写一个诗歌，关于冬天",
+            "晚上睡不着应该怎么办",
+        ]
+        for input in text_list:
+            response, history = model.generate_for_continue_writing(tokenizer, input, history=[], max_length=2048,
+                                           eos_token_id=config.eos_token_id,
+                                           do_sample=True, top_p=0.7, temperature=0.95, )
+            print("input", input)
+            print("response", response)
